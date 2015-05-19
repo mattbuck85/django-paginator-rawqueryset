4d4da49753ebc770d88c856d59a9127e4b0f949e
Matt Buck <seamusmb@gmail.com>
2015-05-19 16:39:07 -0400

    Further cleanup of paramater naming started by askaliuk

--- rawpaginator/paginator.py
+++ rawpaginator/paginator.py
@@ -7,8 +7,8 @@ class DatabaseNotSupportedException(Exception):
 
 class RawQuerySetPaginator(DefaultPaginator):
     "An efficient paginator for RawQuerySets."
-    def __init__(self,object_list,per_page,orphans=0,allow_empty_page_first=True):
-        super(RawQuerySetPaginator,self).__init__(object_list,per_page,orphans,allow_empty_page_first)
+    def __init__(self,object_list,per_page,orphans=0,allow_empty_first_page=True):
+        super(RawQuerySetPaginator,self).__init__(object_list,per_page,orphans,allow_empty_first_page)
         self.raw_query_set = self.object_list
         self.connection = connections[self.raw_query_set.db]
 
