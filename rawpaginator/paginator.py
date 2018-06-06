4ec0fa7d291a9a648abdd2ed7c9b0e5c5546f1a9
Matt Buck <7415944+mblance@users.noreply.github.com>
2018-06-06 07:08:36 -0400

    Args Asbtraction

--- rawpaginator/paginator.py
+++ rawpaginator/paginator.py
@@ -11,8 +11,8 @@ class RawQuerySetPaginator(DefaultPaginator):
     """
     _count = None
 
-    def __init__(self, object_list, per_page, orphans=0, allow_empty_first_page=True):
-        super(RawQuerySetPaginator, self).__init__(object_list, per_page, orphans, allow_empty_first_page)
+    def __init__(self, *args, **kwargs):
+        super(RawQuerySetPaginator, self).__init__(*args, **kwargs)
         self.raw_query_set = self.object_list
         self.connection = connections[self.raw_query_set.db]
 
